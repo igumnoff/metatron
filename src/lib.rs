38bf876bb55e55c9a3508b645bc967490d8f84c3
Evgeny Igumnov <igumnovnsk@gmail.com>
2024-04-03 18:59:59 +0500

    polish

---
+++ src/lib.rs
@@ -0,0 +1,44 @@
+use std::collections::HashMap;
+use bytes::Bytes;
+use serde_yaml::Value;
+use thiserror::Error;
+
+pub struct Report;
+
+
+impl Report {
+
+    pub fn generate(template: &str, data: &str,  images: &HashMap<String, Bytes>) -> anyhow::Result<Bytes> {
+        let data: Value = serde_yaml::from_str(template)?;
+        let data1 = data.get("title").ok_or(ReportError::Common)?;
+        println!("{:?}", data1);
+        println!("{:?}", data1.as_sequence().ok_or(ReportError::Common)?.first());
+        Ok(Bytes::from(""))
+    }
+
+}
+
+#[derive(Error, Debug)]
+pub enum ReportError {
+    #[error("Report error")]
+    Common,
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::collections::HashMap;
+
+    #[test]
+    fn test_generate() -> anyhow::Result<()> {
+        let template_vec = std::fs::read("data/report-template.yaml")?;
+        let template = std::str::from_utf8(&template_vec).unwrap();
+        let data_vec = std::fs::read("data/report-data.json")?;
+        let data = std::str::from_utf8(&data_vec).unwrap();
+        let images = HashMap::new();
+        let result = Report::generate(template, data, &images);
+        assert!(result.is_ok());
+        Ok(())
+    }
+}
