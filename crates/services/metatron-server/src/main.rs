eef8e1e6aad80ba3ad925e514325430df91882f8
Bhuwan Pandit <bhuwanpandit109@gmail.com>
2024-09-19 09:18:24 +0100

    feat: add clap and support -p and --port flag to bind address to the given port otherwise falls back to default port

--- crates/services/metatron-server/src/main.rs
+++ crates/services/metatron-server/src/main.rs
@@ -1,8 +1,33 @@
 use metatron_server::router;
+use clap::Parser;
+
+
+#[derive(Parser, Debug)]
+#[command(
+    name = "metatron",
+    author,
+    version,
+    about = "Metatron: Implementation in Rust of a report generation based on Shiva library",
+    long_about = None
+)]
+struct Args {
+    #[arg(
+        short,
+        long, 
+        default_value_t = 3000, 
+        help = "The port number to bind the server to",
+    )]
+    port: u16,
+}
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    let args = Args::parse();
+
+    let address = format!("0.0.0.0:{}", args.port);
+
+    let listener = tokio::net::TcpListener::bind(&address).await.unwrap();
     axum::serve(listener, router()).await.unwrap();
 }
